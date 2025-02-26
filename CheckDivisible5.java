97875d6b4891d9555a10a11d0f61bdb91aa98823
sairam-14 <velpulasairam51@gmail.com>
2025-02-26 23:00:55 +0530

    Add files via upload

---
+++ CheckDivisible5.java
@@ -0,0 +1,11 @@
+class  CheckDivisible5
+{
+	public static void main(String[] args) 
+	{
+		int a =10;
+
+		String output =(a%5==0)?a+"Divisible by 5":a+"Not Divisible by 5";
+
+		System.out.println(output);
+	}
+}
