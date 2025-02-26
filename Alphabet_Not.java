159d9e76d5f98f5c2d656ee4d856468b37537d95
sairam-14 <velpulasairam51@gmail.com>
2025-02-26 22:53:22 +0530

    Add files via upload

---
+++ Alphabet_Not.java
@@ -0,0 +1,12 @@
+class Alphabet_Not 
+{
+	public static void main(String[] args) 
+	{
+		char x = 'A';
+		if((x>='A'&&x<='Z')||(x>='a'&&x<='z')){
+			System.out.println(x +"is Alphabet");
+		}else{
+			System.out.println(x +"is not Alphabet");
+		}
+	}
+}
